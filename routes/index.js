c3c40fd1d5823d92b3672a479ae365e3f18e9293
RngValue <104089072+RngValue@users.noreply.github.com>
2023-12-17 11:32:18 +0100

    lecturer page prepared

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET lecturer page. */
+router.get('/lecturer', function(req, res, next) {
+  res.render('lecturer', { title: 'Express' });
+});
+
 module.exports = router;

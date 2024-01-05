d99daf9d5dcba0be158876c1059167cf98977328
RngValue <104089072+RngValue@users.noreply.github.com>
2024-01-05 19:49:24 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -146,12 +146,12 @@ router.put('/lecturers/:uuid', function(req, res, next) {
     sql = `SELECT * FROM lecturer WHERE uuid LIKE '%${req.params.uuid}%'`
     let oldresult
     db.all(sql, [], (err, rows) => {
-      if (err) return console.error(err)
+      if (err) return res.status(404).json({ status: 404, success: false, })
       try {
+        oldresult = rows[0]
         if (rows[0].hasOwnProperty('tags')) { rows[0].tags = JSON.parse(rows[0].tags) }
         if (rows[0].hasOwnProperty('contact')) { rows[0].contact = JSON.parse(rows[0].contact) }
       } catch (error) { return res.status(404).json({ status: 404, success: false, })}
-      oldresult = rows[0]
     })
     sql = `UPDATE lecturer SET `
     if (req.body.hasOwnProperty('title_before')) { sql += `title_before = '${title_before}', ` }

50686d2ef2cec1866a46aefc1fc5a65f9abb32d4
RngValue <104089072+RngValue@users.noreply.github.com>
2024-01-05 19:19:05 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -149,8 +149,6 @@ router.put('/lecturers/:uuid', function(req, res, next) {
       db.all(sql, [], (err, rows) => {
         if (err) return console.error(err)
         try {
-          if (rows[0].hasOwnProperty('tags')) { rows[0].tags = JSON.parse(rows[0].tags) }
-          if (rows[0].hasOwnProperty('contact')) { rows[0].contact = JSON.parse(rows[0].contact) }
           oldresult = rows[0]
         } catch (error) { return res.status(404).json({ status: 404, success: false, })}
       })

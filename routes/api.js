31b2e481958dc7f402e9723e4608a18418bb4d43
RngValue <104089072+RngValue@users.noreply.github.com>
2024-01-05 17:06:19 +0100

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -70,6 +70,8 @@ router.post('/lecturers', function(req, res, next) {
       price_per_hour,
       contact
       ) VALUES (?,?,?,?,?,?,?,?,?,?,json(?),?,json(?))`
+    if (!req.body.hasOwnProperty('first_name')) return res.status(400).json({ status: 400, success: false, })
+    if (!req.body.hasOwnProperty('last_name')) return res.status(400).json({ status: 400, success: false, })
     if (uuid == null) uuid = crypto.randomUUID()
     if (req.body.hasOwnProperty('tags')) {
       for (var x = 0; x < tags.length; x++) {

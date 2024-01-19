76b7538d6ede7d6aac6e26fd678e5a18f6a83576
Static <angiebusinessacc@gmail.com>
2024-01-19 22:36:21 +0100

    Shrine added suksesfulej
    not all features but eh

--- public/js/shrine.js
+++ public/js/shrine.js
@@ -1,1 +1,12 @@
-//Gonna put shit for shrine here ;3
+function shrineswap(){
+    element= document.getElementById("shrine");
+    if (element.src.match("/images/shrine/shrine.png")){
+        element.src = "/images/shrine/shrine_open.png";
+    } else {
+        element.src = "images/shrine/shrine.png";
+    }
+    var audio = new Audio('images/shrine/royaltyfree.mp3')
+    audio.play()
+    audio.loop = true;
+    audio.volume = 0.2;
+}

83e078ff617bc611a634a730e239b0f3ed4ec4fb
RngValue <104089072+RngValue@users.noreply.github.com>
2024-02-24 17:38:43 +0100

    forgor change urls

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 var doc = document
-fetch(`http://localhost:3000/api/lecturers`)
+fetch(`http://4e6843e4cd0e2b27.app.tourdeapp.cz/api/lecturers`)
    .then((response) => response.json())
    .then((json) => {
       // let tagssss = ""

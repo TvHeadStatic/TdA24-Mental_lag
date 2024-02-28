1006cda09acf9204a40ed197886583fc3fa1b4d9
RngValue <104089072+RngValue@users.noreply.github.com>
2024-02-28 19:27:27 +0100

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -4,22 +4,22 @@ const signupForm = document.querySelector('.signupform')
 
 loginForm.addEventListener('submit', event => {
    event.preventDefault()
-   const formData = new FormData(loginForm)
-   const data = Object.fromEntries(formData)
+   const loginFormData = new FormData(loginForm)
+   const data = Object.fromEntries(loginFormData)
    window.open(`http://4e6843e4cd0e2b27.app.tourdeapp.cz/dashboard/${data.uuid}`, "_self");
 })
 
 signupForm.addEventListener('submit', event => {
    event.preventDefault()
-   const formData = new FormData(loginForm)
-   const data = Object.fromEntries(formData)
+   const signupFormData = new FormData(loginForm)
+   const data = Object.fromEntries(signupFormData)
 
    //post
    fetch("http://4e6843e4cd0e2b27.app.tourdeapp.cz/api/lecturers", {   
       method: "POST",
       body: JSON.stringify({
-         first_name: String(formData.firstname),
-         last_name: String(formData.lastname)
+         first_name: data.firstname,
+         last_name: data.lastname
       }),
       headers: {
          "Content-type": "application/json; charset=UTF-8",

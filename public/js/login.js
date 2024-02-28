3412bf33e340002e6ed149d56736bf905052c847
RngValue <104089072+RngValue@users.noreply.github.com>
2024-02-28 19:53:27 +0100

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -13,14 +13,25 @@ signupForm.addEventListener('submit', event => {
    event.preventDefault()
    const signupFormData = new FormData(signupForm)
    const data = Object.fromEntries(signupFormData)
-
+   let RNG = Math.floor(Math.random() * 3);
+   let personal_pepe = "http://4e6843e4cd0e2b27.app.tourdeapp.cz/images/TdA_ikony/300ppi/TdA_ikony_napad_black.png"
+   switch(RNG) {
+      case 0:
+         personal_pepe = "http://4e6843e4cd0e2b27.app.tourdeapp.cz/images/TdA_ikony/300ppi/TdA_ikony_nastaveni_black.png"
+      case 1:
+         personal_pepe = "http://4e6843e4cd0e2b27.app.tourdeapp.cz/images/TdA_ikony/300ppi/TdA_ikony_studium_black.png"
+   }
    //post
    fetch("http://4e6843e4cd0e2b27.app.tourdeapp.cz/api/lecturers", {   
       method: "POST",
       body: JSON.stringify({
          first_name: String(data.firstname),
          last_name: String(data.lastname),
-         tags: [{"name": "Default"},{"name": "Newcomer"}]
+         tags: [{"name": "Default"},{"name": "Newcomer"}],
+         price_per_hour: 24.99,
+         bio: "I am a lecturer available for purchase on Teacher digital Agency!",
+         location: "Somewhere",
+         picture_url: personal_pepe
       }),
       headers: {
          "Content-type": "application/json; charset=UTF-8",
